18ce7dfa6c28c49ca3bb3a40ba434fed11e91a09
No_thoughts <89064032445@mail.ru>
2023-03-18 01:21:51 +0300

    + Docker - Перенес строку подключения в .env файл

--- MyProfile/Services/EnvConfiguration.cs
+++ MyProfile/Services/EnvConfiguration.cs
@@ -6,15 +6,14 @@ public static class EnvConfiguration
     private static string? _mySQL;
     private static string ConfigureURLMYSQLFromEnviroment()
     {
-        var address = Environment.GetEnvironmentVariable("MYSQL_SERVER");
-        var port = Environment.GetEnvironmentVariable("MYSQL_PORT");
-        var userId = Environment.GetEnvironmentVariable("MYSQL_USER");
-        var password = Environment.GetEnvironmentVariable("MYSQL_PASSWORD");
+        var address      = Environment.GetEnvironmentVariable("MYSQL_SERVER");
+        var port         = Environment.GetEnvironmentVariable("MYSQL_PORT");
+        var userId       = Environment.GetEnvironmentVariable("MYSQL_USER");
+        var password     = Environment.GetEnvironmentVariable("MYSQL_PASSWORD");
         var databaseName = Environment.GetEnvironmentVariable("MYSQL_DATABASE");
 
         var result = $@"Server={address};Port={port};Database={databaseName};Uid={userId};Pwd={password};";
         Console.WriteLine($"Connection string : {result}");
         return _mySQL = result;
     }
-   
 }
