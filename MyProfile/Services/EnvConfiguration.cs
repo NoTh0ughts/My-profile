ee97d1c1c5695fcdb822a9ca9cb1fe51e8c17f33
No_thoughts <89064032445@mail.ru>
2024-09-23 08:37:03 +0300

    Добавил загрузку конфига для Redis

--- MyProfile/Services/EnvConfiguration.cs
+++ MyProfile/Services/EnvConfiguration.cs
@@ -2,9 +2,22 @@
 
 public static class EnvConfiguration
 {
-    public static string MySqlUrl => _mySQL ??= ConfigureURLMYSQLFromEnviroment();
-    private static string? _mySQL;
-    private static string ConfigureURLMYSQLFromEnviroment()
+    public static string MySqlUrl => _mySql ??= ConfigureMysqlFromEnvironment();
+    public static string RedisConf => _redisConf ??= ConfigureRedisConf();
+
+    private static string ConfigureRedisConf()
+    {
+        var address = Environment.GetEnvironmentVariable("SERVER");
+        var port    = Environment.GetEnvironmentVariable("PORT");
+        var password= Environment.GetEnvironmentVariable("REDIS_PASSWORD");
+        var result = $"{address}:{port},password={password}";
+        return result;
+    }
+
+    private static string? _mySql;
+    private static string? _redisConf;
+
+    private static string ConfigureMysqlFromEnvironment()
     {
         var address      = Environment.GetEnvironmentVariable("MYSQL_SERVER");
         var port         = Environment.GetEnvironmentVariable("MYSQL_PORT");
@@ -13,7 +26,6 @@ public static class EnvConfiguration
         var databaseName = Environment.GetEnvironmentVariable("MYSQL_DATABASE");
 
         var result = $@"Server={address};Port={port};Database={databaseName};Uid={userId};Pwd={password};";
-        Console.WriteLine($"Connection string : {result}");
-        return _mySQL = result;
+        return _mySql = result;
     }
 }
